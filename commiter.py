0621feafe450f424c08f19c8d9c0306f3fc3c032
Fake User <email@example.com>
2023-10-28 16:54:49 +0200

    Your commit message here

--- commiter.py
+++ commiter.py
@@ -6,11 +6,23 @@ if len(sys.argv) != 2:
     print("Usage: python script.py <remote_name>")
     sys.exit(1)
 
+
+
 # Get the remote name from the command-line argument
 remote_name = sys.argv[1]
 
 # Open the Git repository in the current directory
 repo = git.Repo(".")
+
+
+# Get the author's name and email from the Git configuration
+config = repo.config_reader()
+author_name = config.get_value("user", "name")
+author_email = config.get_value("user", "email")
+
+
+print(f"Author Name: {author_name}")
+print(f"Author Email: {author_email}")
 
 custom_author = git.Actor("Your Custom Author Name", "custom.author@example.com")
 
